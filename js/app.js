495e329aab662191dd94c70628ffb34bde9e2d30
[Martina Klima] <[martinaklimova0@gmail.com]>
2017-02-23 21:06:27 +0100

    Place all enemy objects in an array called allEnemies

--- js/app.js
+++ js/app.js
@@ -2,15 +2,17 @@
 var Enemy = function() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-
+    this.speed = 200;
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.reset();
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
+
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
@@ -24,12 +26,23 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+ Player.prototype.reset = function(){
+    this.y=410;
+    this.x=200;
+};
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
+var enemy1 = new Enemy();
+var enemy2 = new Enemy();
+var enemy3 = new Enemy();
+var allEnemies = [enemy1, enemy2, enemy3];
+
+
 // Place the player object in a variable called player
 
+var player = new Player();
 
 
 // This listens for key presses and sends the keys to your
@@ -44,3 +57,4 @@ document.addEventListener('keyup', function(e) {
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
+
